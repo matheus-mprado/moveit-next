b6f4d5a0d1aa2412380b39c43d968f02b348e9d5
Matheus Maximiliano <maximiliano.mprado@gmail.com>
2021-02-27 08:18:22 -0300

    commit

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import { ReactNode } from 'react';
-
-interface AuthData{
-    request:ReactNode;
-    response:ReactNode;
-}
+import axios from 'axios';
 
 export default function Auth(){
 
-    async function getCode(){
+    interface MapData{
+        item:ReactNode;
+        key:Number;
+    }
+
         const router  =  useRouter();
-        
         const  code  =  router.query.code;
+    
         const clientId = 'f48e3c48965bbf6160e4';
         const client_secret = 'e7e1bb896eab1207fa2ee3e98f06d7980c568602 '
 
@@ -21,24 +21,20 @@ export default function Auth(){
             client_secret:client_secret,
             code: code,
         }
+        const headers = {
+            'Accept': 'application/json',
+            'Content-Type':'application/json'
+        }
 
-        const req = await fetch(`https://github.com/login/oauth/access_token`,{
-            method:'POST',
-            headers:{
-                Accept: 'application/json',
-            },
-            body: JSON.stringify(body)
-        }).
-        then(()=>{
-            console.log(req);
-        })
-    }
-    
-    getCode();
+        async function teste(){
+            const response = await axios.post('https://github.com/login/oauth/access_token',body,{headers});
+        }
+        
+       
 
     return(
         <div>
-            
+            {body.code}
         </div>
     );
 }
